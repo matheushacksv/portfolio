b0f47ecc76f22da70095defb627a0a46cc630e37
Matheus Hack <matheushacksv@gmail.com>
2026-03-18 09:04:21 -0300

    feat: Configure Django to serve media files in development.

--- core/settings.py
+++ core/settings.py
@@ -135,6 +135,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 COMPRESSOR_ROOT = BASE_DIR / 'static'
 
 COMPRESSOR_ENABLED = True
